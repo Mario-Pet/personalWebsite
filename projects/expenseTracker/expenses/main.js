6d7fe56786c6268032127660c8c4e0dd728cb80b
Mario-Pet <mpetkovic691@gmail.com>
2022-01-15 13:47:14 +0100

    made progress on the currency exchanger, fixed bugs with the statuses, working on table overflow

--- projects/expenseTracker/expenses/main.js
+++ projects/expenseTracker/expenses/main.js
@@ -10,7 +10,7 @@ const balance = {
         balance.current += Number(n)
         document.querySelector("#balance").textContent = Intl.NumberFormat("fr-FR").format(balance.current)
     },
-    currency: "$"
+    currency: "€"
 }
 
 const statusDecider = {
@@ -43,7 +43,7 @@ document.querySelector("form").addEventListener("submit", (e) => {
     })
 
     document.querySelector("table tbody").append(row);
-    console.log(statusCell)
+
     row.append(
         ...Array.from(document.querySelectorAll("table th:not(:last-child")).map((th) => 
         createElementWithOptions("td", {
@@ -51,37 +51,69 @@ document.querySelector("form").addEventListener("submit", (e) => {
         })),
         statusCell
     );
+
     if (statusCell.textContent === "pending") {
         statusCell.setAttribute("class", "pending")
     }
+
     if (statusses.length > 1) {
         setTimeout(() => {
             statusCell.textContent = statusses[1];
             statusCell.classList.remove(statusses[0]);
 
-            if (statusses.includes("approved"))
-            balance.add(data.get("amount"));
-            statusCell.setAttribute("class", "approved")
+            if (statusses.includes("approved")) {
+                statusCell.setAttribute("class", "approved")
+
+                if (balance.currency === "$") {
+                    var amount = data.get("amount")
+                    balance.add(amount * 1.1452051406);
+                } else if (balance.currency === "€") {
+                    balance.add(data.get("amount"))
+                }
+            } else {
+                statusCell.setAttribute("class", "denied")
+            }
+            
         }, 5000)
     } else {
-        if (statusses.includes("approved"))
-        balance.add(data.get("amount"));
+        if (statusses.includes("approved")) {
+            if (balance.currency === "$") {
+                var amount = data.get("amount")
+                balance.add(amount * 1.1452051406);
+            } else if (balance.currency === "€") {
+                balance.add(data.get("amount"))
+            }
+        }
+
+        if (statusses.includes("denied")) {
+            statusCell.setAttribute("class", "denied")
+        }
     }
+      e.target.reset()  
+    })
     
 
-    e.target.reset();
-})
+   
 
 document.querySelector("#currencySelector").addEventListener("click", () => {
-    let currentCurrency = document.querySelector("#currencySelector").value
+    balance.currency = document.querySelector("#currencySelector").value
 })
 
 function onChangeCurrency() {
     let newCurrency = document.querySelector("#currencySelector").value
 
-    if (currentCurrency === "€" && newCurrency === "$") {
-        balance.current = balance.current * 1.1361002677
-        document.querySelector("#balance").textContent = balance.current;
+    if (balance.currency === "€" && newCurrency === "$") {
+        balance.current = balance.current * 1.1452051406;
+        document.querySelector("#balance").textContent = balance.current.toFixed(2);
+        document.querySelector("#currency").textContent = "$";
+        document.querySelector("#balance").textContent = Intl.NumberFormat("fr-FR").format(balance.current)
+    } else if (newCurrency == null) {
+        balance.add(data.get("amount"))
     }
+
+    if (balance.currency === "$" && newCurrency === "€") {}
 }
 
+console.log(312312 * 1.1415303584);
+console.log(1 -0.14153035840000006)
+console.log(357661.308 * 0.8784696415999999)
