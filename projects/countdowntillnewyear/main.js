5f179f6a595966e418287d27fb15af47f1d5620b
Mario-Pet <mpetkovic691@gmail.com>
2022-01-16 18:03:32 +0100

    Shortening the code for best practices and readability

--- projects/countdowntillnewyear/main.js
+++ projects/countdowntillnewyear/main.js
@@ -15,7 +15,7 @@ setInterval(() => {
     let minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
     let seconds = Math.floor(distance % (1000 * 60) / 1000);
     
-    // Assigning the values to the corresponding 
+    // Assigning the values to the corresponding html elements
     let daysId = document.querySelector("#days")
     daysId.textContent = days;
     let hoursId = document.querySelector("#hours")
