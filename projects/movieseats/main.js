6e1f7a01f33ed3afabb6cc519311f1052b948fd0
Mario-Pet <mpetkovic691@gmail.com>
2022-01-28 15:44:03 +0100

    added more structure

---
+++ projects/movieseats/main.js
@@ -0,0 +1,37 @@
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+const chairs = {
+    numberOfChairs: 0,
+    numberOfRandomChairs: 0,
+    add(num) {
+        const oddOrEven = () => {
+
+        }
+
+        const leftSide = () => {
+
+        }
+
+        const middleSide = () => {
+            
+        }
+
+        const rightSide = () => {
+            
+        }
+    },
+    randomChairSelector() {}
+}
+
+const sentenceManipulator = {
+    chairs: [],
+    price: 0
+}
+
+const email = {
+    value: "hey@mariopetkovic.com",
+    looks() {},
+    send() {}
+}
